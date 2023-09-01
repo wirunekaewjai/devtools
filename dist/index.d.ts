ccb4e0f681c8aed4dcb2730f9ba2976cfa388628
Wirune Kaewjai <wirune.kae@hotmail.com>
2023-09-01 15:49:32 +0700

    add types export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 import { buildAndWatchAssets } from './build-and-watch-assets';
 import { buildAssets } from './build-assets';
-export { buildAndWatchAssets, buildAssets, };
+import { Config, ConfigInput } from './utils/create-config';
+export { Config, ConfigInput };
+export { buildAndWatchAssets, buildAssets };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { buildAndWatchAssets } from '@/src/build-and-watch-assets';
 import { buildAssets } from '@/src/build-assets';
+import { Config, ConfigInput } from '@/src/utils/create-config';
+
+export {
+  Config,
+  ConfigInput
+};
 
 export {
   buildAndWatchAssets,
-  buildAssets,
+  buildAssets
 };
